b5bf0a31edf7353f133b3adb2183155da1d7bcdb
ebajec <tacoman164@gmail.com>
2025-09-04 19:31:06 +0000

    less jank

--- configure.sh
+++ configure.sh
@@ -1,14 +1,39 @@
 #!/usr/bin/bash
 
+CONTAINER_VER=false
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    --container-ver)
+      CONTAINER_VER=true
+      shift
+      ;;
+    *)
+      echo "Unknown option: $1"
+      exit 1
+      ;;
+  esac
+done
+
 bash ./scripts/nvim-setup.sh
 bash ./scripts/tmux-setup.sh
 bash ./scripts/zsh-setup.sh
 
-stow\
-	--adopt\
-	-t $HOME\
-	home\
-	alacritty\
-	nvim\
-	tmux\
-	zsh\
+if $CONTAINER_VER; then
+	stow\
+		--adopt\
+		-t $HOME\
+		home\
+		nvim\
+		tmux\
+		zsh
+else 
+	stow\
+		--adopt\
+		-t $HOME\
+		home\
+		alacritty\
+		nvim\
+		tmux\
+		zsh
+fi
--- scripts/zsh-setup.sh
+++ scripts/zsh-setup.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/jeffreytse/zsh-vi-mode \
-  $ZSH_CUSTOM/plugins/zsh-vi-mode
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  	echo "Oh My Zsh already installed"
+else
+	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-
+if  [ -d "$ZSH_CUSTOM/plugins/zsh-vi-mode" ]; then
+  	echo "zsh-vi-mode already installed"
+else
+  	echo "Installing zsh-vi-mode ..."
+	git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode
+fi
